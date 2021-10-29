72d27a398fa033ecc94b9864f5a44111c3e5f004
Yurii Hrecheniuk <yuriy@hrecheni.uk>
2021-10-29 23:37:50 +0300

    added onDestroy unsubscriptions

--- src/app/components/show-description/show-description.component.ts
+++ src/app/components/show-description/show-description.component.ts
@@ -1,14 +1,15 @@
-import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {ShowService} from '../../services/show.service';
 import {IShow} from '../../models/show.interface';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-show-description',
   templateUrl: './show-description.component.html',
   styleUrls: ['./show-description.component.scss']
 })
-export class ShowDescriptionComponent implements OnChanges {
+export class ShowDescriptionComponent implements OnChanges, OnDestroy {
 
   @Input() showId!: number;
   @Input() showType!: 'tv' | 'movie';
@@ -17,15 +18,21 @@ export class ShowDescriptionComponent implements OnChanges {
 
   show!: IShow;
 
+  subscription!: Subscription;
+
   constructor(private movieService: ShowService) {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.movieService.getShow(this.showType, this.showId)
+    this.subscription = this.movieService.getShow(this.showType, this.showId)
       .subscribe(show => {
         this.show = show;
         this.posterUrl = environment.IMG_URL + show.poster_path;
         this.genres = show.genres.map(genre => genre.name);
       });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
